c4cbcceb54ecda8677fef1a930e29e00a235ffd3
Ivan Mauricio Serrano Pradilla <1107846851@u.icesi.edu.co>
2025-02-21 13:00:10 -0500

    entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -26,20 +26,42 @@ public class Atlas {
 	{   
         // Declaracion de info a pedir del usuario (completar)
         String nombre;
+        String ciudad;
         // Saludo
         System.out.println("Bienvenido al programa de viajes EVENTOS ATLAS realizado por los estudiantes de Algoritmos y programacion I de la Universidad ICESI.");
         // Pedir info al usuario (completar)
         System.out.println("Cual es tu nombre?");
         nombre = escaner.nextLine();
+        System.out.println("¿Que edad tienes?");
+        int edad = escaner.nextInt();
+        System.out.println("Ingrese su numero de documento");
+        int doc = escaner.nextInt();
+        System.out.println("¿Desde que ciudad nos contactas?");
+        ciudad = escaner.nextLine();
+        System.out.println("Bienvenido a EVENTOS ATLAS, Cuantas noches desea hospedarse? (minimo 1 - maximo 4)");
+        int noches = escaner.nextInt();
+        System.out.println("BUS(1) o AVION(2) ");
+        int trans = escaner.nextInt();
+
+
+
 
         // ...
-        System.out.println("Bienvenido " + nombre + "!");
+        
         // ...
 
         // Declaracion de salidas calculadas (completar)
         double totalTransporte;
+        double totalHospedaje;
         // Calculo de salidas mediante metodos (completar)
-        totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
+        totalTransporte = calculartotalTransporte(); // aqui podrian faltar parametros
+        totalHospedaje = calculartotalHospedaje(noches + 150000);
+
+        System.out.println("Bienvenido " + nombre + "!");
+        System.out.println("Edad: " + edad);
+        System.out.println("Documento: " + doc);
+        System.out.println("El total del hospedaje: " + totalHospedaje);
+
 	}
 
 	public static void main(String[] args) {
